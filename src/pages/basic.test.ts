cba5ac8e2b3bca204f3cfa8070eec3b620c9b6b4
dvelosojob <veloso.deivid.freelance@gmail.com>
2025-06-11 10:31:23 -0300

    feat: install vitest

---
+++ src/pages/basic.test.ts
@@ -0,0 +1,22 @@
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import { expect, test } from "vitest";
+import Home from "./index.astro";
+
+test("Home page should contain Tailwind button", async () => {
+  const container = await AstroContainer.create();
+  const result = await container.renderToString(Home);
+
+  expect(result).toContain("Tailwind Button in Astro!");
+  expect(result).toContain(
+    '<button class="appearance-none py-2 px-4 bg-purple-500'
+  );
+});
+
+test("Home page should contain markdown link", async () => {
+  const container = await AstroContainer.create();
+  const result = await container.renderToString(Home);
+
+  expect(result).toContain('href="/markdown-page"');
+  expect(result).toContain("Markdown is also supported...");
+  expect(result).toContain('class="p-4 underline"');
+});
